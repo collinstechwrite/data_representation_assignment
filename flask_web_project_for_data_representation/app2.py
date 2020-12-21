dcc0b92e2d65dcdc7a05ea7e39c0de50f7c5be81
g00387822 <G00387822@gmit.ie>
2020-12-21 21:39:57 +0000

    added a delete database command

--- flask_web_project_for_data_representation/app2.py
+++ flask_web_project_for_data_representation/app2.py
@@ -87,8 +87,97 @@ def my_form_post():
 
 
 
+@app.route('/delete_database')
+def delete_database():
+
+    DELETE FROM headlinetitles;
+
+   #Port number: 3306
+
+    cur = mysql.connection.cursor()
+    cur.execute('''DELETE FROM headlinetitles;''')
+    results = cur.fetchall()
+    results = str(results)
+    return """ 
+    <style>
+    table.blueTable {
+  border: 1px solid #1C6EA4;
+  background-color: #EEEEEE;
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+}
+table.blueTable td, table.blueTable th {
+  border: 1px solid #AAAAAA;
+  padding: 3px 2px;
+}
+table.blueTable tbody td {
+  font-size: 13px;
+}
+table.blueTable tr:nth-child(even) {
+  background: #D0E4F5;
+}
+table.blueTable thead {
+  background: #1C6EA4;
+  background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  border-bottom: 2px solid #444444;
+}
+table.blueTable thead th {
+  font-size: 15px;
+  font-weight: bold;
+  color: #FFFFFF;
+  border-left: 2px solid #D0E4F5;
+}
+table.blueTable thead th:first-child {
+  border-left: none;
+}
+
+table.blueTable tfoot {
+  font-size: 14px;
+  font-weight: bold;
+  color: #FFFFFF;
+  background: #D0E4F5;
+  background: -moz-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  background: -webkit-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  background: linear-gradient(to bottom, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  border-top: 2px solid #444444;
+}
+table.blueTable tfoot td {
+  font-size: 14px;
+}
+table.blueTable tfoot .links {
+  text-align: right;
+}
+table.blueTable tfoot .links a{
+  display: inline-block;
+  background: #1C6EA4;
+  color: #FFFFFF;
+  padding: 2px 8px;
+  border-radius: 5px;
+}
+</style>   
+    
+    <table class="blueTable"><thead><tr><th><centre>Database Contents On MySQL Server</centre></th></tr></thead><tbody><tr><td>""" + results + """ </td></tr></tbody></tr></table>
+    <img src="static/images/plot.png" alt="A Matplotlibplot">
+    <button onclick="goBack()">Go Back</button>
+
+<script>
+function goBack() {
+  window.history.back();
+}
+</script>
+    """ 
+
+
+
 
 @app.route('/database')
+
+
+
+
 def database():
 
    #Port number: 3306
